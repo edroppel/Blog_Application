6b54f43fe137e96f4bcfb8952356f22006669314
edroppel <edroppel@lion.lmu.edu>
2024-12-09 16:16:36 -0800

    final hw3

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,13 @@
 export default function Nav({ articles, setArticle }) {
-  return (
-    <nav>
-      {!articles
-        ? "No articles"
-        : articles.map((a) => (
-            <p key={a.id} onClick={() => setArticle(a)}>
-              {a.title}
-            </p>
-          ))}
-    </nav>
-  )
-}
+    return (
+      <nav>
+        {!articles
+          ? "No articles"
+          : articles.map((a) => (
+              <p key={a.id} onClick={() => setArticle(a)}>
+                {a.title}
+              </p>
+            ))}
+      </nav>
+    )
+  }
